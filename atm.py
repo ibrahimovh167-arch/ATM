e6046d6629839e0d9159e8ffe8d013093927e674
Huseyn Ibrahimov <huseyN259>
2026-08-25 10:36:35 +0400

    new methods added

--- atm.py
+++ atm.py
@@ -28,6 +28,42 @@ class ATM:
         pin = self.pinEntry.get()
 
         if pin == self.pin:
-            pass
+            self.mainScreen()
         else:
             messagebox.showwarning("Warning", "PIN is wrong, try again.")
+
+    def mainScreen(self):
+        Label(self.window, 
+              text="ATM MENU",
+              font=("Arial", 25, "bold")
+              ).pack(pady=30)
+        
+        Button(self.window,
+               text="Show balance",
+               width=20,
+               command=self.showBalance
+               ).pack(pady=5)
+        
+        Button(self.window,
+               text="Withdraw",
+               width=20,
+               command=None
+               ).pack(pady=5)
+        
+        Button(self.window,
+               text="Deposit",
+               width=20,
+               command=None
+               ).pack(pady=5)
+        
+        Button(self.window,
+               text="Exit",
+               width=20,
+               command=None
+               ).pack(pady=20)
+        
+    def showBalance(self):
+        messagebox.showinfo("Info", f"Balance: {self.balance} AZN")
+
+    def withdraw(self):
+        pass
